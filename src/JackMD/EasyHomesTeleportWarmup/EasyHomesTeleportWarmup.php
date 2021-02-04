5d93c3ee3749a398ee660edfe14c188e7322cb79
LdsPE <43183272+LdsPE@users.noreply.github.com>
2021-02-04 13:09:30 -0300

    Turning does not cancel teleportation

--- src/JackMD/EasyHomesTeleportWarmup/EasyHomesTeleportWarmup.php
+++ src/JackMD/EasyHomesTeleportWarmup/EasyHomesTeleportWarmup.php
@@ -19,6 +19,14 @@ class EasyHomesTeleportWarmup extends PluginBase implements Listener{
 	 *
 	 * key => player lower case name
 	 *
+	 * 0 => $block
+	 */
+	private $move = [];
+	/**
+	 * @var array
+	 *
+	 * key => player lower case name
+	 *
 	 * 0 => $player
 	 * 1 => $homeLocation
 	 * 2 => $warmupTime
@@ -48,6 +56,7 @@ class EasyHomesTeleportWarmup extends PluginBase implements Listener{
 		$warmupTime = $config->get("warmup-time", 5);
 		$homeLocation = $event->getHomeLocation();
 
+		$this->move[$player->getLowerCaseName()] = $player->getLevel()->getBlock($player->subtract(0, 1, 0));
 		$this->warmup[$player->getLowerCaseName()] = [
 			$player,
 			$homeLocation,
@@ -73,18 +82,24 @@ class EasyHomesTeleportWarmup extends PluginBase implements Listener{
 
 	public function onMove(PlayerMoveEvent $event){
 		$player = $event->getPlayer();
+		$to = $event->getTo();
 
 		if(!isset($this->warmup[$player->getLowerCaseName()])){
 			return;
 		}
 
-		unset($this->warmup[$player->getLowerCaseName()]);
+		$move = $this->move[$player->getLowerCaseName()];
+		$block = $player->getLevel()->getBlock($to->subtract(0, 1, 0));
+		if($move != $block){
+			unset($this->move[$player->getLowerCaseName()]);
+			unset($this->warmup[$player->getLowerCaseName()]);
 
-		if($player->hasEffect(Effect::NAUSEA)){
-			$player->removeEffect(Effect::NAUSEA);
+			if($player->hasEffect(Effect::NAUSEA)){
+				$player->removeEffect(Effect::NAUSEA);
+			}
+
+			$player->sendMessage($this->getConfig()->getNested("you-moved", "Teleporting cancelled since you moved."));
 		}
-
-		$player->sendMessage($this->getConfig()->getNested("you-moved", "Teleporting cancelled since you moved."));
 	}
 
 	public function getWarmups(): array{
@@ -96,6 +111,7 @@ class EasyHomesTeleportWarmup extends PluginBase implements Listener{
 	}
 
 	public function removeWarmup(Player $player): void{
+		unset($this->move[$player->getLowerCaseName()]);
 		unset($this->warmup[$player->getLowerCaseName()]);
 	}
 }
